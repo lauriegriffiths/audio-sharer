49a5aa423dbae4a485c19ea9476a3a15054b74ef
lauriegriffiths <laurieinjapan@gmail.com>
2020-04-16 16:18:51 +0900

    Changed Mic Plugin

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./styles.css";
 import firebase from "firebase/app";
 import "firebase/storage";
-
-const MicRecorder = require("mic-recorder-to-mp3");
+import { ReactMic } from "@cleandersonlobo/react-mic";
 
 var firebaseConfig = {
   apiKey: "AIzaSyDUHOZ9nNIHihQdd5bwe0cDxXkCeAhG46I",
@@ -12,55 +11,29 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 class App extends React.Component {
-  recorder = new MicRecorder({
-    bitRate: 128
-  });
   constructor(props) {
     super(props);
     this.state = {
       recording: false,
-      audioFile: null,
       filename: "default"
     };
   }
 
-  startRecording = () => {
-    this.recorder
-      .start()
-      .then(() => {
-        // something else
-      })
-      .catch(e => {
-        console.error(e);
-      });
+  onStartClicked = () => {
+    this.setState({ recording: true });
+  };
+  onStopClicked = () => {
+    this.setState({ recording: false });
   };
 
-  stopRecording = () => {
-    this.recorder
-      .stop()
-      .getMp3()
-      .then(([buffer, blob]) => {
-        // do what ever you want with buffer and blob
-        // Example: Create a mp3 file and play
-        const file = new File(buffer, "me-at-thevoice.mp3", {
-          type: blob.type,
-          lastModified: Date.now()
-        });
+  uploadFile = recordedBlob => {
+    console.log("recordedBlob is: ", recordedBlob);
+    var storageRef = firebase.storage().ref();
+    var fileRef = storageRef.child("audio/" + this.state.filename + ".mp3");
 
-        const player = new Audio(URL.createObjectURL(file));
-        this.setState({ audioFile: URL.createObjectURL(file) });
-
-        var storageRef = firebase.storage().ref();
-        var fileRef = storageRef.child("audio/" + this.state.filename + ".mp3");
-
-        fileRef.put(file).then(function(snapshot) {
-          console.log("Uploaded a blob or file!");
-        });
-      })
-      .catch(e => {
-        alert("We could not retrieve your message");
-        console.log(e);
-      });
+    fileRef.put(recordedBlob.blob).then(function(snapshot) {
+      console.log("Uploaded a blob or file!");
+    });
   };
 
   fileNameChanged = e => {
@@ -72,8 +45,19 @@ class App extends React.Component {
       <div className="App">
         <h1>Mic upload test</h1>
         <input onChange={this.fileNameChanged} />
-        <button onClick={this.startRecording}>record</button>
-        <button onClick={this.stopRecording}>stop</button>
+        <button onClick={this.onStartClicked}>record</button>
+        <button onClick={this.onStopClicked}>stop</button>
+        <div>
+          <ReactMic
+            record={this.state.recording} // defaults -> false.  Set to true to begin recording
+            onStop={this.uploadFile} // callback to execute when audio stops recording
+            onData={() => {}} // callback to execute when chunk of audio data is available
+            strokeColor="#000000"
+            backgroundColor="#FF4081"
+            className="sound-wave"
+            mimeType={"audio/mp3"} // defaults -> audio/wav. Set audio/mp3 to switch to MP3     // defaults -> 44100. It accepts values only in range: 22050 to 96000
+          />
+        </div>
       </div>
     );
   }
